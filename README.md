5ac1fc04c29739b2d93bf177b0088fdd86807a67
Nosova_LA <drzombas@yandex.ru>
2023-07-14 10:48:10 +0300

    homework 2 lesson

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # LevelUP
 LevelUP
 my first commit
+
+Тест 1: 18/20
+Тест 2: 15/20
---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <Windows.h>
+
+using namespace std;
+
+int main()
+{
+    cout << "Start" << endl;
+    for (int i=0; i<50; i++){
+        cout << "-" ;
+        fflush (stdout);
+        Sleep (500);
+    }
+    cout << "100%" << endl;
+    cout << "Stop" << endl;
+    return  0;
+}
