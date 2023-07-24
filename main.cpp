bc891d3da336cb5c1eadda5bec97f49dbdd9a092
Nosova_LA <drzombas@yandex.ru>
2023-07-24 09:18:27 +0300

    Lesson 4

--- main.cpp
+++ main.cpp
@@ -1,17 +1,47 @@
-#include <iostream>
-#include <Windows.h>
+#include <QFile>
+#include <QTextStream>
+#include <QDebug>
 
-using namespace std;
 
-int main()
-{
-    cout << "Start" << endl;
-    for (int i=0; i<50; i++){
-        cout << "-" ;
-        fflush (stdout);
-        Sleep (500);
-    }
-    cout << "100%" << endl;
-    cout << "Stop" << endl;
-    return  0;
-}
+class ReadWriteFile {
+public:
+    bool ReadFile (const QString &NameFile){ //ф-ия чтения текстового файла
+        QFile file (NameFile);               // создание объекта
+        if (!file.open(QIODevice::ReadOnly | QIODevice::Text)) //проверка
+            return false;
+
+        QTextStream in(&file); // создание текстового потока
+        in.setCodec("UTF-8");  // кодировка
+
+        while (!in.atEnd())   // чтение из файла
+        {
+            QString line = in.readLine();
+            qDebug()<<line;
+        }
+        file.close(); // закрытие файла
+        return true;
+    } // конец ф-ии чтения
+
+    bool WrireFile (const QString &NameFile,const QString &StringLine){ //ф-ия записи текстового файла
+        QFile file (NameFile);  // создание объекта
+        if (!file.open(QIODevice::WriteOnly | QIODevice::Text)) //проверка
+            return false;
+
+        QTextStream out(&file); // создание текстового потока
+        out.setCodec("UTF-8");  // кодировка
+
+        out << StringLine << endl; // запись
+        file.close(); // закрытие файла
+        return true;
+    } // конец ф-ии записи
+};
+
+
+int main() {
+    ReadWriteFile txt;
+    txt.WrireFile("Test.txt","Hello world");
+    txt.ReadFile("Test.txt");
+    return 0;
+};
+
+
